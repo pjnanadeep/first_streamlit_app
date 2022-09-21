ea451629118543a3b30d6fc53ff041ab850ae308
pjnanadeep <60767847+pjnanadeep@users.noreply.github.com>
2022-09-21 14:03:07 -0700

    Update streamline_app.py
    Fixed typo

--- streamline_app.py
+++ streamline_app.py
@@ -7,6 +7,6 @@ streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('pyKale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 
-streamlit.text('pyHard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
