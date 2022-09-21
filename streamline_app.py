db2d1d1a85383f015bc21af950453b7ad8bb05f2
pjnanadeep <60767847+pjnanadeep@users.noreply.github.com>
2022-09-21 14:13:27 -0700

    Update streamline_app.py
    Added emoji's and smoothie header

--- streamline_app.py
+++ streamline_app.py
@@ -3,10 +3,10 @@ import streamlit
 
 streamlit.title('My New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header( 'Breakfast Favorites')
+streamlit.text('🥣 omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit. text('🥑 Avocado Toast')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
