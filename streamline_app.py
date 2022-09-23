c4d80fccff20618904d3d8807574bc811e589297
pjnanadeep <60767847+pjnanadeep@users.noreply.github.com>
2022-09-23 14:31:07 -0700

    Update streamline_app.py
    Fixed syntax error

--- streamline_app.py
+++ streamline_app.py
@@ -31,4 +31,4 @@ streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 # take the json version of the response and normalize it
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
-streamlit.dataframe(fruityvicenormalized)
+streamlit.dataframe(fruityvice_normalized)
